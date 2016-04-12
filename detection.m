4e37d8058d61aaaf7d7773ee5964aef4ce323111
Karthik Vijay <vijaypcguy@gmail.com>
2016-04-13 01:31:53 +0530

    pca gives error rate of 50%, change to lda

--- detection.m
+++ detection.m
@@ -22,12 +22,32 @@ endfor
 
 %Testing phase
 
-for i = 0:39
-	j = i*10 + 1;
-	temp = double(rotdim(d(:,j:j+4))) * v;
-	meanFaces(i+1,:) = mean(temp,1);
+errorRate = 0;
+
+for l = 0:4
+	for i = 0:39
+		j = i*10 + 6 + l;
+		newVector = double(rotdim(d(:,j))) * v;
+		
+		minimumDistance = norm(meanFaces(1,:) - newVector,2);
+		person = 1;
+
+		for k = 2:40
+			temp = norm(meanFaces(k,:) - newVector,2);
+
+			if (minimumDistance > temp)
+				minimumDistance = temp;
+				person = k;	
+			endif
+
+		endfor
+
+		if( person != i+1)
+			errorRate++;
+		endif
+	endfor
 endfor
 
-
+errorRate
 
 
